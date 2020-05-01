a67266f0f532d2dcc90e21b1cf632281de0d746c
Szilveszter Dobak <dobaksz@gmail.com>
2020-05-01 22:58:58 +0200

    Bump version to 1.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! rsntp = "1.0.0"
+//! rsntp = "1.0.1"
 //! ```
 //!
 //! Obtain the current local time with the blocking API:
@@ -53,7 +53,7 @@ dependency to `tokio` which reduces crate dependencies significantly.
 
 ```toml
 [dependencies]
-rsntp = { version = "1.0.0", default-features = false }
+rsntp = { version = "1.0.1", default-features = false }
 ```
 "##
 )]
